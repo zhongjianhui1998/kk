d73377a963d02185e701d770560893f521122b36
zhongjianhui1998 <59462608+zhongjianhui1998@users.noreply.github.com>
2020-01-06 09:22:07 +0800

    Create test4.java
    新建的第二个类

---
+++ src/test4.java
@@ -0,0 +1,22 @@
+package test2;
+
+public class test {
+	public static void main(String[] args) {
+		System.out.println("脰脨鹿煤戮眉脠脣");
+		System.out.println("脰脨鹿煤戮眉脠脣");
+		System.out.println("脰脨鹿煤戮眉脠脣");
+		System.out.println("脰脨鹿煤戮眉脠脣");
+		System.out.println("脰脨鹿煤戮眉脠脣");
+		System.out.println("脰脨鹿煤戮眉脠脣");
+		System.out.println("脰脨鹿煤戮眉脠脣");
+		System.out.println("脰脨鹿煤戮眉脠脣");
+		System.out.println("脰脨鹿煤戮眉脠脣");
+		System.out.println("脰脨鹿煤戮眉脠脣");
+		System.out.println("脰脨鹿煤戮眉脠脣");
+		System.out.println("脰脨鹿煤戮眉脠脣");
+		System.out.println("脰脨鹿煤戮眉脠脣");
+		System.out.println("脰脨鹿煤戮眉脠脣");
+		System.out.println("脰脨鹿煤戮眉脠脣");
+		System.out.println("脰脨鹿煤戮眉脠脣");
+	}
+}
